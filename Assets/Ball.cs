9f9943cd5fa242aab17f206e4aff97a1c1068b30
Gary <fishellgary@gmail.com>
2020-02-29 09:14:10 -0800

    Initial project commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private bool hasStarted;
+    private Rigidbody2D ball;
+    public int initialSpeed;
+    private float ballSpeed;
+    public float ballIncSpeed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasStarted = false;
+        ball = GetComponent<Rigidbody2D>();
+        ballSpeed = initialSpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1") && !hasStarted)
+        {
+
+            float direction = Random.Range(-1, 1);
+            Vector2 initialForce = new Vector2();
+
+            if (direction > 0)
+            {
+                initialForce.x = initialSpeed;
+                initialForce.y = 0;
+            }
+
+            else
+            {
+                initialForce.x = -(initialSpeed);
+                initialForce.y = 0;
+            }
+
+            ball.AddForce(initialForce);
+            ball.gravityScale = 0.5F;
+
+            hasStarted = true;
+        }
+
+        //if (Mathf.Abs(ball.velocity.x) != ballSpeed || Mathf.Abs(ball.velocity.y) != ballSpeed && hasStarted) {
+        //    Vector2 currentDirection = new Vector2();
+
+        //    if (ball.velocity.x < 0)
+        //    {
+        //        currentDirection.x = -(ballSpeed);
+        //    }
+
+        //    else if (ball.velocity.y < 0) {
+
+        //        currentDirection.y = -(ballSpeed);
+        //    }
+
+        //    else if (ball.velocity)
+        //    else
+        //    {
+        //        currentDirection.x = initialSpeed;
+        //    }
+
+        //    currentDirection.y = ball.velocity.y;
+
+        //    ball.velocity = currentDirection;
+        //}
+
+        if (hasStarted)
+        {
+            Vector2 currentDirection = new Vector2();
+
+            currentDirection.x = ball.velocity.x < 0 ? -(ballSpeed) : ballSpeed;
+            currentDirection.y = ball.velocity.y < 0 ? -(ballSpeed) : ballSpeed;
+
+            ball.velocity = currentDirection;
+        }
+
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if ( col.gameObject.name == "Paddle1" || col.gameObject.name == "Paddle2" ) {
+            ballSpeed += ballIncSpeed;
+        }    
+    
+    }
+
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Vector2 change;
+    private Vector2 currentPos;
+    public bool isComputer;
+    private Rigidbody2D ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        currentPos = body.position;
+        ball = GameObject.Find("Ball").GetComponent<Rigidbody2D>();
+
+
+        if(!isComputer)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isComputer)
+        {
+            change.y = Input.GetAxisRaw("Mouse Y") + currentPos.y;
+            currentPos.y = change.y;
+            change.x = currentPos.x;
+            body.MovePosition(change);
+        }
+
+        else if (Mathf.Abs(ball.position.y) - currentPos.y > .05)
+        {
+            Vector2 newPos = new Vector2(currentPos.x, currentPos.y + (ball.position.y - currentPos.y));
+            body.MovePosition(newPos);
+        }
+
+        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        body.velocity = Vector2.zero;
+    }
+}
